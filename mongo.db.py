a63fdaaa9f3eaa245e97d7b2eaf765c1f5c4b902
pmahajan <m.prasshantmahajan@gmail.com>
2022-07-26 12:29:36 +0530

    This is my first coad

---
+++ mongo.db.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://pmahajan:mXqIHVJZ0p5FkM6W@cluster0.albf6.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+a={
+    "name" : "prashant",
+    "email.id" : "m.prashantmahajan@gmail.com",
+    "password" : "India@2022"
+}
+db1=client["mongotest"]
+coll=db1['test']
+coll.insert_one(a)
+
